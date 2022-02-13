c0f775ebcbcfff822de23ba5d18e1f97342dfdc9
ankit-mrexcalibur <55681020+ankit-mrexcalibur@users.noreply.github.com>
2022-02-13 15:28:10 +0530

    commit

--- src/components/connect/Connect.jsx
+++ src/components/connect/Connect.jsx
@@ -4,19 +4,19 @@ const Connect=()=> {
     return (
         <div className="cn">
             <a href="https://www.linkedin.com/in/ankitkk22/" target="_blank">
-                <img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-linkedin-social-media-justicon-flat-justicon.png" className="cn-icon"/>
+                <img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-linkedin-social-media-justicon-flat-justicon.png" alt="" className="cn-icon"/>
             </a>
             <a href="https://www.facebook.com/ankitkumarkamat22" target="_blank">
-                <img src="https://img.icons8.com/fluency/48/000000/facebook-new.png" className="cn-icon"/>
+                <img src="https://img.icons8.com/fluency/48/000000/facebook-new.png" alt="" className="cn-icon"/>
             </a>
             <a href="https://www.instagram.com/disintegrated.engineer/" target="_blank">
-                <img src="https://img.icons8.com/fluency/48/000000/instagram-new.png" className="cn-icon"/>
+                <img src="https://img.icons8.com/fluency/48/000000/instagram-new.png" alt="" className="cn-icon"/>
             </a>
             <a href="https://github.com/ankit-mrexcalibur" target="_blank">
-                <img src="https://img.icons8.com/color/48/000000/github--v1.png" className="cn-icon"/>
+                <img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="" className="cn-icon"/>
             </a>
             <a href="https://twitter.com/er_ankitkamat" target="_blank">
-                <img src="https://img.icons8.com/color/48/000000/twitter--v1.png" className="cn-icon"/>
+                <img src="https://img.icons8.com/color/48/000000/twitter--v1.png" alt="" className="cn-icon"/>
             </a>
         </div>
     )
