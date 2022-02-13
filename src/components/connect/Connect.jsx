c5c21e656973d29c58fd4128f8d4ff8c6c42b0f5
ankit-mrexcalibur <55681020+ankit-mrexcalibur@users.noreply.github.com>
2022-02-13 15:33:24 +0530

    Removed target=blank from Connect.jsx

--- src/components/connect/Connect.jsx
+++ src/components/connect/Connect.jsx
@@ -3,19 +3,19 @@ import "./connect.css";
 const Connect=()=> {
     return (
         <div className="cn">
-            <a href="https://www.linkedin.com/in/ankitkk22/" target="_blank" rel="noreferrer">
+            <a href="https://www.linkedin.com/in/ankitkk22/">
                 <img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-linkedin-social-media-justicon-flat-justicon.png" alt="" className="cn-icon"/>
             </a>
-            <a href="https://www.facebook.com/ankitkumarkamat22" target="_blank" rel="noreferrer">
+            <a href="https://www.facebook.com/ankitkumarkamat22">
                 <img src="https://img.icons8.com/fluency/48/000000/facebook-new.png" alt="" className="cn-icon"/>
             </a>
-            <a href="https://www.instagram.com/disintegrated.engineer/" target="_blank" rel="noreferrer">
+            <a href="https://www.instagram.com/disintegrated.engineer/">
                 <img src="https://img.icons8.com/fluency/48/000000/instagram-new.png" alt="" className="cn-icon"/>
             </a>
-            <a href="https://github.com/ankit-mrexcalibur" target="_blank" rel="noreferrer">
+            <a href="https://github.com/ankit-mrexcalibur" target="_blank">
                 <img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="" className="cn-icon"/>
             </a>
-            <a href="https://twitter.com/er_ankitkamat" target="_blank" rel="noreferrer">
+            <a href="https://twitter.com/er_ankitkamat" target="_blank">
                 <img src="https://img.icons8.com/color/48/000000/twitter--v1.png" alt="" className="cn-icon"/>
             </a>
         </div>
